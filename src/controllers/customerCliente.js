a6824e700fbf62be13e4f4f9223b8f15e01d6bce
ecro <ecro98.98@gmail.com>
2023-07-28 17:05:33 -0500

    Agregar Animación a Pantalla Principal (Lordicon)-(clientes.ejs) Set Controllers de manera que podamos identificarlos de mejor manera (clientes.js-customerCliente.js) Nuevas Rutas (clientes.js-customerCliente.js) Crear Archivo Factura.ejs para vizualizar las facturas y agregar los datos necesarios a las vistas. Header.ejs agregar nuevas rutas Footer.ejs agregar cdn lordicon

--- src/controllers/customerCliente.js
+++ src/controllers/customerCliente.js
@@ -2,11 +2,11 @@
 const listClientesController={};
 listClientesController.list= (req, res)=>{
   req.getConnection((err, conn)=>{
-    conn.query('select * from armazon', (err, cliente)=>{
+    conn.query('select * from cliente', (err, cliente)=>{
       if(err){
         res.json(err)
       }
-      console.log(cliente)
+      //console.log(cliente)
       res.render('clientes', {
         data: cliente
       })
@@ -15,12 +15,34 @@ listClientesController.list= (req, res)=>{
 };
 
 const clienteController={};
-
 clienteController.save=(req, res)=>{
   const data= req.body;
   req.getConnection((err, conn)=>{
     conn.query('INSERT INTO cliente set ?', [data], (err, cliente)=>{
-      console.log(err)
+      //console.log(err)
+      res.redirect('/');
+    })
+  })
+}
+
+const addLente={};
+addLente.aggLe=(req, res)=>{
+  const datalente=req.body;
+  req.getConnection((err, conn)=>{
+    conn.query('insert into marca set ?', [datalente], (err, marca)=>{
+      //console.log(err);
+      res.redirect('/');
+    })
+  })
+}
+
+
+const addLuna={};
+addLuna.aggLu=(req, res)=>{
+  const dataLuna = req.body;
+  req.getConnection((err, conn)=>{
+    conn.query('insert into tipoluna set ?', [dataLuna], (err, marca)=>{
+      //console.log(err);
       res.redirect('/');
     })
   })
@@ -28,8 +50,59 @@ clienteController.save=(req, res)=>{
 
 
 
+const facturas = {};
+facturas.crear = (req, res) => {
+    const idFactura = req.params;
+    //console.log(idFactura)
+
+    // Hacer una consulta a la tabla marca
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM marca', (err, marcas) => {
+            //console.log(marcas);
+
+            // Hacer una consulta a la tabla tamaño
+            conn.query('SELECT * FROM tamano', (err, tamanos) => {
+                //console.log(tamaños);
+
+                // Renderizar los resultados en la vista factura
+                res.render('factura', {
+                    idFactura: idFactura,
+                    marcas: marcas,
+                    tamanos: tamanos
+                });
+            });
+        });
+    });
+};
+
+module.exports = facturas;
+
+
+module.exports = facturas;
+
+
+
+
+
+const crearFactura={};
+crearFactura.make=(req, res)=>{
+  const idCliente=req.body;
+  console.log(idCliente);
+  req.getConnection((err, conn)=>{
+    conn.query('insert into factura set ?', [idCliente], (err, facturas)=>{
+      const idFactura=facturas.insertId;
+      res.redirect('/factura/'+idFactura);
+
+    })
+
+  })
+}
 
 module.exports= {
   listClientesController,
-  clienteController
+  clienteController,
+  addLente,
+  addLuna,
+  facturas,
+  crearFactura
 };
--- src/routes/clientes.js
+++ src/routes/clientes.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const router= express.Router();
-const {listClientesController, clienteController}= require('../controllers/customerCliente')
-
+const router = express.Router();
+const {
+    listClientesController,
+    clienteController,
+    addLente,
+    addLuna,
+    facturas,
+    crearFactura
+} = require('../controllers/customerCliente')
 
 
 router.get('/', listClientesController.list);
-
 router.post('/addCliente', clienteController.save);
+router.post('/addLente', addLente.aggLe);
+router.post('/addTipoLuna', addLuna.aggLu);
 
 
+//Factura
+router.get('/factura/:id', facturas.crear)
+router.post('/crearFactura', crearFactura.make);
 
 
-
-module.exports= router;
+module.exports = router;
